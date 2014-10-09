0a61266228490c9196ab1557ae90342949b8dde5
Aaron Dancer <aarondancer@Aarons-MBP.attlocal.net>
2014-10-08 20:49:35 -0500

    Initial Commit

---
+++ Placeholder Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Placeholder Test
+//
+//  Created by Aaron Dancer on 10/8/14.
+//  Copyright (c) 2014 Aaron Dancer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
